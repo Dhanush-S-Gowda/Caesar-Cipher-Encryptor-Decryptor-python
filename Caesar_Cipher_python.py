8f44a0493856a5c17a0e2589235e60ce05cb4181
Dhanush S Gowda <141866615+Dhanush-S-Gowda@users.noreply.github.com>
2024-01-26 01:10:54 +0530

    Update Caesar_Cipher_python.py

--- Caesar_Cipher_python.py
+++ Caesar_Cipher_python.py
@@ -10,23 +10,21 @@ print(welcome, "\n")
 print("Welcome to Caesar Cipher encryptr/Decoder\n")
 
 alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', '|', ';', ':', "'", '"', '<', '>', ',', '.', '/', '?', ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 def caesar(text, shift, type):
     cipher = ""
+    length = len(alphabet_list)
+    if type == "d":
+        shift *= -1
+        length *= -1
     for i in text:
-        if i in special_characters:
+        if i not in alphabet_list:
             cipher += i
             continue
         position = alphabet_list.index(i)
-        if type == "e":
-            new_position = position + shift
-            if new_position>= len(alphabet_list):
-                new_position -= len(alphabet_list)
-        elif type == "d":
-            new_position = position - shift
-            if new_position< 0:
-                new_position += len(alphabet_list)
+        new_position = position + shift
+        if (new_position>= len(alphabet_list)) or (new_position< 0):
+            new_position %= length
         cipher += alphabet_list[new_position]
     return cipher
 
@@ -46,7 +44,7 @@ while True:
     else:
         print("Invalid input try again.")
         continue
-
+        
     status = input("\nEnter 'y' to continue or 'n' to exit: ").lower()
     if status == 'y':
         continue
